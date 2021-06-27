63d2742b8bcd3a0201046129297e35afdc8be4dd
Safira Adani <75374315+fiwa12@users.noreply.github.com>
2021-06-27 12:47:48 +0700

    Add files via upload

---
+++ Front-End-Client/src/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { ToastProvider } from "react-toast-notifications";
+import { createStore } from "redux";
+import rootReducer from "./redux/reducers/rootReducer";
+
+const reduxStore = createStore(
+  rootReducer,
+  +window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ToastProvider placement='top-right'>
+        <Provider store={reduxStore}>
+          <App />
+        </Provider>
+      </ToastProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
